5cc16f37acef02126796a0d3a46ad2004835371f
Fabian Canas <fcanas@gmail.com>
2015-02-13 13:36:00 -0500

    Create Frameworks for Carthage support

---
+++ ObjectMapping/OHMKit-OSX/OHMKit-OSX.h
@@ -0,0 +1,14 @@
+//
+//  OHMKit-OSX.h
+//  OHMKit-OSX
+//
+//  Created by Fabian Canas on 2/13/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double OHMKit_OSXVersionNumber;
+FOUNDATION_EXPORT const unsigned char OHMKit_OSXVersionString[];
+
+#import <OHMKit/OHMKit.h>
